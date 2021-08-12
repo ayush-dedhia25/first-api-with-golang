20d2dddca89257b065ca8b3a7700a126b0842246
ayush-dedhia25 <ayushdedhia25@gmail.com>
2021-08-12 11:39:30 +0530

    Removed some files visiblity

--- users/routes.go
+++ users/routes.go
@@ -7,7 +7,7 @@ import (
 func UserRoutes(app *fiber.App) {
    app.Get("/users", GetAllUsers)
    app.Get("/users/:id", GetUser)
-   app.Post("/users/:id", CreateUser)
+   app.Post("/users", CreateUser)
    app.Put("/users/:id", UpdateUser)
    app.Delete("/users/:id", DeleteUser)
 }
